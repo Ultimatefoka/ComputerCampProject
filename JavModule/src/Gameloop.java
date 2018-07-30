cdad7ae8944f643bd4fd30d9880e6bea531b4050
Moritz <mobuwer28@googlemail.com>
2018-07-30 17:36:36 +0200

    Main added

--- JavModule/src/Gameloop.java
+++ JavModule/src/Gameloop.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 
-public class Gameloop {
+public class Gameloop implements Runnable{
 
     private boolean running=true;
 
+    @Override
     public void run(){
 
         while(running){
