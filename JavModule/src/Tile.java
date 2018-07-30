922c3d32a176f27ed96993dfa030b95cf3f4f050
niklvolk18 <niklas.volkweib@gmail.com>
2018-07-30 12:25:40 +0200

    test

--- JavModule/src/Tile.java
+++ JavModule/src/Tile.java
@@ -4,8 +4,35 @@ public class Tile {
     private String name;
     private boolean begehbar;
 
-    public Tile(String grafik) {
+    public Tile(String grafik, String name, boolean begehbar) {
 
+        this.grafik = grafik;
+        this.name = name;
+        this.begehbar = begehbar;
         
+    }
+
+    public void setGrafik(String grafik) {
+        this.grafik = grafik;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBegehbar(boolean begehbar) {
+        this.begehbar = begehbar;
+    }
+
+    public String getGrafik() {
+        return grafik;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isBegehbar() {
+        return begehbar;
     }
 }
