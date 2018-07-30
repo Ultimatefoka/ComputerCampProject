45367b957bd75fc432b9e73c891292f719d16988
Moritz <mobuwer28@googlemail.com>
2018-07-30 17:43:10 +0200

    Show Gameframe

--- JavModule/src/Gameloop.java
+++ JavModule/src/Gameloop.java
@@ -1,14 +1,19 @@
+import Rendering.GameFrame;
+
 import java.util.ArrayList;
 
 public class Gameloop implements Runnable{
 
     private boolean running=true;
-
+    private GameFrame gameframe;
+    public Gameloop() {
+        gameframe = new GameFrame("CoolesSpiel", 64 * 12, (64 * 8) + 20);
+    }
     @Override
     public void run(){
-
+        gameframe.setVisible(true);
         while(running){
-
+            GameManager.getInstance().update();
             try {
 
                 Thread.sleep(40);
