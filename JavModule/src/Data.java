e1ba028528a3161735cbde5f2eb1397d2e7e3fd1
finnkarp18 <finn.arne.k@googlemail.com>
2018-07-30 18:14:13 +0200

    Data

---
+++ JavModule/src/Data.java
@@ -0,0 +1,28 @@
+import java.io.File;
+
+public class Data {
+
+    private static Data dataIntance;
+    private static GameAssets gameAssetsInstance;
+
+    public static Data getDataIntance() {
+
+        if(dataIntance == null) {
+
+            dataIntance = new Data();
+        }
+
+        return dataIntance;
+    }
+
+    public static GameAssets getGameAssetsInstance() {
+
+        if(gameAssetsInstance == null) {
+
+            File dataFile = new File("assets/data.json");
+
+        }
+
+        return gameAssetsInstance;
+    }
+}
