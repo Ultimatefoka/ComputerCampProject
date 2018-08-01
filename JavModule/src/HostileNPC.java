ff0e0214c329dad214196992a81eb0e312b4b615
niklvolk18 <niklas.volkweib@gmail.com>
2018-08-01 11:32:44 +0200

    hostile and neutral NPC

---
+++ JavModule/src/HostileNPC.java
@@ -0,0 +1,12 @@
+public class HostileNPC extends NPC {
+
+    private int health;
+
+    public HostileNPC (String graphic, String name, int x, int y, String map, int health) {
+
+        super(graphic, name, x, y, map);
+        this.health = health;
+
+    }
+
+}
