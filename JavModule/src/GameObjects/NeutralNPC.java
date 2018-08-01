5cf98e0455ea62c394acae1a0b71296402f6c8e0
niklvolk18 <niklas.volkweib@gmail.com>
2018-08-01 12:45:40 +0200

    new package

--- JavModule/src/GameObjects/NeutralNPC.java
+++ JavModule/src/GameObjects/NeutralNPC.java
@@ -1,11 +1,38 @@
 package GameObjects;
+import java.util.concurrent.TimeUnit;
 
 public class NeutralNPC extends NPC {
 
-    public NeutralNPC(String graphic, String name, int x, int y, String map) {
+    private boolean busy = false;
+    private int distanceX;
+    private int distanceY;
+
+    public NeutralNPC(String graphic, String name, int x, int y, String map, int distanceX, int distanceY) {
 
         super(graphic, name, x, y, map);
+        this.distanceX = distanceX;
+        this.distanceY = distanceY;
 
     }
 
+    public void createPathing() {
+
+        this.addWaypoint(distanceX, distanceY);
+        this.addWaypoint(-distanceX, -distanceY);
+
+    }
+
+    public void move() {
+
+
+
+    }
+
+    public void setBusy(boolean busy) {
+        this.busy = busy;
+    }
+
+    public boolean getBusy() {
+        return busy;
+    }
 }
