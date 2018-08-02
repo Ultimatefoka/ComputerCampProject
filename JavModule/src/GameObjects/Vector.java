76019c16535d46dc78566e4e178b908b5e8a4726
niklvolk18 <niklas.volkweib@gmail.com>
2018-08-02 17:37:39 +0200

    Physics based movement

---
+++ JavModule/src/GameObjects/Vector.java
@@ -0,0 +1,47 @@
+package GameObjects;
+
+public class Vector {
+
+    private float x;
+    private float y;
+
+    public Vector(float x, float y) {
+
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public Vector add(Vector vector) {
+        return new Vector(this.getX() + vector.getX(), this.getY() + vector.getY());
+    }
+
+    public Vector substract(Vector vector) {
+        return new Vector(this.getX() - vector.getX(), this.getY() - vector.getY());
+    }
+
+    public Vector scale(float s) {
+        return new Vector(this.getX() * s, this.getY() * s);
+    }
+
+    public float crossProduct(Vector vector) {
+        return (this.getX() * vector.getY() - vector.getX() - this.getY());
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.x = x;
+    }
+
+    public float getX() {
+        return this.x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+}
